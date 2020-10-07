7d1ae690a4dd91c3e9a9bfbd3183435a28ec9741
unknown <[maysaorash@gmail.com]>
2020-10-06 21:29:20 -0400

    add human age to colculation dog years

---
+++ 01-Animal-Years - Project/script.js
@@ -0,0 +1,23 @@
+//calculate dog years
+//let myAge = 3;
+let myAge = prompt('what is your?') *1;
+//The first two years of a dog’s life count as 10.5 dog years each.
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//later years result has been set
+let laterYears = myAge - 2;
+//Each year following equates to 4 dog years.
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//ages are stored
+let myAgeInDogYears = earlyYears + laterYears ;
+let myName = 'John';
+//print name in toLowerCase
+console.log(myName.toLowerCase());
+//displays my name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human 
+    years which is ${myAgeInDogYears} years old in dog years.`);
+    alert(`My name is ${myName}. I am ${myAge} years old in human 
+    years which is ${myAgeInDogYears} years old in dog years.`);
+   
